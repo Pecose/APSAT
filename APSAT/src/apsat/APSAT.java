54090b0736b9158b4fa836f54defa67dd427b8a2
Pecose <pecose.billos@gmail.com>
2021-04-18 10:48:18 +0200

    Update APSAT.java

--- APSAT/src/apsat/APSAT.java
+++ APSAT/src/apsat/APSAT.java
@@ -4,6 +4,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -19,6 +21,7 @@ public class APSAT{
 	public int nbClauses;
 	public int nbVariables;
 	public int nbAffectations = 1;
+	public ArrayList<ArrayList<Integer>> P = new ArrayList<ArrayList<Integer>>();
 	public ArrayList<boolean[]> M = new ArrayList<boolean[]>();
 	public ArrayList<boolean[]> K = new ArrayList<boolean[]>();
 //	Scanner scanner = new Scanner(System.in);
@@ -32,9 +35,9 @@ public class APSAT{
 	
 	public APSAT(String adress) {
 		this.address = adress;
-		System.out.println("Traitement des données...");
 //		System.out.println("Entrez le nombre d'affectations attendu:");
 //		this.nbAffectations = scanner.nextInt();
+		System.out.println("Traitement des données...");
 		this.readFile(address);
 	}
 	
@@ -59,27 +62,52 @@ public class APSAT{
 		            	matcher.find();
 		            	nbVariables = Integer.valueOf(matcher.group());
 		            }else{
-		            	M.add(new boolean[nbVariables*2]);  
+		            	P.add(new ArrayList<Integer>()); 
 		            	do{
 		            		int i = Integer.valueOf(matcher.group());
-		            		if(i > 0){
-		        				M.get(M.size()-1)[Math.abs(i)*2-2] = false;
-		        				M.get(M.size()-1)[Math.abs(i)*2-1] = true;
-		        			}else if(i < 0){
-		        				M.get(M.size()-1)[Math.abs(i)*2-2] = true;
-		        				M.get(M.size()-1)[Math.abs(i)*2-1] = true;
-		        			}
+		            		P.get(P.size()-1).add(i);
 		            	}while(matcher.find()); 
 		            }
 	            }
 		    }
 		    fReader.close();
 		    bReader.close();
+		    
+		    
+		    this.rangerP();
+		    this.PtoM();
 		    this.trouverAffectationsValidePourM();
 		    this.afficherAffectationsValides(); 
 		    timer = (System.nanoTime() - timer) / 1000000000;
 		    System.out.println("Temps écoulé pour traitement des données: "+timer+"s");
 		} catch (Exception e) { e.printStackTrace(); }     
+	}
+
+	public void rangerP() {
+		
+		Comparator<ArrayList<Integer>> sizeComparator = new Comparator<ArrayList<Integer>>() {
+			@Override
+			public int compare(ArrayList<Integer> a, ArrayList<Integer> b) {
+				 return Integer.compare(a.size(), b.size());
+			}
+	    };
+
+	    Collections.sort(P, sizeComparator);
+	}
+	
+	public void PtoM() {
+		for(ArrayList<Integer> p : P){
+			M.add(new boolean[nbVariables*2]); 
+			for(int i : p){
+				if(i > 0){
+					M.get(M.size()-1)[Math.abs(i)*2-2] = false;
+					M.get(M.size()-1)[Math.abs(i)*2-1] = true;
+				}else if(i < 0){
+					M.get(M.size()-1)[Math.abs(i)*2-2] = true;
+					M.get(M.size()-1)[Math.abs(i)*2-1] = true;
+				}
+			}
+		}
 	}
 	
 	public void trouverAffectationsValidePourM() {
@@ -120,19 +148,25 @@ public class APSAT{
 	public void afficherAffectationsValides() {
 		StringBuffer buffer = new StringBuffer();
 		System.out.println();
-		System.out.println("Affectations Valides:");
-		for(boolean[] k : K){
-			for(int l = 0; l < this.nbVariables*2; l+=2) {
-				if(k[l+1]) {
-					if(k[l]){
-						buffer.append((l/2+1) + " ");
-					}else {
-						buffer.append(-(l/2+1) + " ");
+		if(K.size() > 0) {
+			System.out.println("Affectations Valides:");
+			for(boolean[] k : K){
+				for(int l = 0; l < this.nbVariables*2; l+=2) {
+					if(k[l+1]) {
+						if(k[l]){
+							buffer.append((l/2+1) + " ");
+						}else {
+							buffer.append(-(l/2+1) + " ");
+						}
 					}
 				}
+				buffer.append("0 \n");
 			}
-			buffer.append("0 \n");
+			System.out.println(buffer.toString());
+		}else {
+			System.out.println("Aucune affectation Valide");
 		}
-		System.out.println(buffer.toString());
+		
 	}
 }
+
